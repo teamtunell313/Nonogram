1a6766f53eb881f3d1dc2db4d692d9bc8ef0a157
Sean Tunell <teamtunell313@gmail.com>
2022-07-08 22:37:37 -0600

    greatly simplified cypress file, clicks in puzzle too

--- cypress/integration/examples/nonogram.spec.js
+++ cypress/integration/examples/nonogram.spec.js
@@ -1,16 +1,11 @@
 /* eslint-disable no-console */
 /* eslint-disable max-len */
-import LineSolver from '../../../src/DivideOpenings';
+import {divideSeries} from '../../../src/divideSeries';
 import {parseHeaders} from '../../../src/helper'
 // const { wait, getQueriesForElement } = require("@testing-library/react");
 
 /* eslint-disable no-undef */
 Cypress.on('uncaught:exception', (err, runnable) => false);
-// returning false here prevents Cypress from
-// failing the test
-
-// npm install cypress --save-dev
-// npm run cypress
 
 it('fetch nonogram top and side data into tables', () => {
   cy.visit('https://www.puzzle-nonograms.com/?size=1');
@@ -18,109 +13,56 @@ it('fetch nonogram top and side data into tables', () => {
 
   cy.get('.task-group').then(($taskGrps) => { // Get column and row header divs
     const puzzle = parseHeaders($taskGrps)
-    console.log({puzzle});
+    // console.log({puzzle});
+    /* puzzle: {
+      dimension: 5,
+      headers: [
+        { complete: false,
+          determined: 9,
+          guide: [1, 1, 1, 3],
+          needsUpdate: true
+        },
+        { ... },
+        { ... }
+      ]
+    }
+    */
 
-    // // setup the solution array: 0 is blank, -1 is X, 1 is Blot.
-    // const solution = new Array(dimension * dimension).fill(0);
+    // create the solution array: 0 is blank, -1 is X, 1 is Blot.
+    const solution = Array.from(Array(puzzle.dimension), () => new Array(puzzle.dimension).fill(0))
+    do { // at least one header isn't complete
+      for (let i = 0; i < puzzle.dimension*2; i++) { // loop through headers
+        if(!puzzle.headers[i].complete){ // skip complete headers
+          if(i < puzzle.dimension) { // header is a column
+            let series = solution.map(row => row[i])
+            let header = {...puzzle.headers[i], series}
+            // console.log(`column ${i} w/ hint ${puzzle.headers[i].guide} & series ${series}`);
+            header = divideSeries(header)
+            puzzle.headers[i] = header
+            solution.map((row, index) => row[i] = header.series[index])
+          } else { // header is a row
+            let series = solution[i-puzzle.dimension]
+            let header = {...puzzle.headers[i], series}
+            // console.log(`row ${i-puzzle.dimension} w/ hint ${header.guide} & series ${series}`)
+            header = divideSeries(header)
+            puzzle.headers[i] = header
+            solution[i-puzzle.dimension] = header.series
+          }
+        }
+      }
+    } while ((puzzle.headers.filter(header => header.complete == false).length > 0));
 
-    // if (headerData.complete.indexOf(false) !== -1) { // puzzle isn't solved if we have an incomplete header
-    //   for (let col = 0; col < dimension; col += 1) { // loop through columns. col = [0, 1, 2, 3, 4]
-    //     if (!headerData.complete[col] && headerData.needsUpdate[col]) { // Only loop through column cells if due for an update
-    //       const lineData = {
-    //         dimension,
-    //         guide: headerData.guide[col],
-    //         determined: headerData.determined[col],
-    //         determinedXs: headerData.determinedXs[col],
-    //         xLocations: [],
-    //         blotLocations: [],
-    //         emptyLocations: []
-    //       };
-    //       for (let cell = col; cell < dimension * dimension; cell += dimension) { // Loop through each column cell [0, 5, 10, 15, 20]
-    //         const columnSequence = (cell - col) / 5; // 0, 1, 2, 3, 4
-    //         switch (solution[cell]) {
-    //           case -1:
-    //             lineData.xLocations.push(columnSequence);
-    //             break;
-    //           case 1:
-    //             lineData.blotLocations.push(columnSequence);
-    //             break;
-    //           default:
-    //             lineData.emptyLocations.push(columnSequence);
-    //         } // end switch
-    //       } // lineData object populated.
+    console.log(`Solution: ${JSON.stringify(solution)}`)
 
-    //       // send data to line solver, and save data it returns to solution array
-    //       const newMarks = LineSolver(lineData);
-    //       newMarks.data.forEach((value, location) => {
-    //         if (value !== 0) {
-    //           const cell = location * 5 + col; // calculate cell index
-    //           const row = 5 + Math.floor(cell / 5); // calculate row index
-    //           solution.splice(cell, 1, value); // place an X or a blot
-    //           headerData.needsUpdate.splice(row, 1, true); // mark the row for an update
-    //         } // no change in this location.
-    //       }); // finished cycling through newMarks Data
-    //       if (newMarks.complete) headerData.complete.splice(col, 1, true); // mark the column complete
-    //     } // column didn't need update.
-    //   } // Looped through all columns.
-
-    //   // loop through the rows
-    //   console.log('loop through rows');
-    //   for (let row = 0; row < dimension; row += 1) { // Loop through each row [0, 1, 2, 3, 4]
-    //     if (!headerData.complete[5 + row] && headerData.needsUpdate[5 + row]) { // Only loop through row cells if due for an update
-    //       const lineData = {
-    //         dimension,
-    //         guide: headerData.guide[5 + row],
-    //         determined: headerData.determined[5 + row],
-    //         determinedXs: headerData.determinedXs[5 + row],
-    //         xLocations: [],
-    //         blotLocations: [],
-    //         emptyLocations: []
-    //       };
-    //       for (let cell = row * 5; cell < row * 5 + dimension; cell += 1) { // Loop through each row cell [0, 1, 2, 3, 4]
-    //         const rowSequence = cell % (row * 5);
-    //         switch (solution[cell]) {
-    //           case -1:
-    //             lineData.xLocations.push(rowSequence);
-    //             break;
-    //           case 1:
-    //             lineData.blotLocations.push(rowSequence);
-    //             break;
-    //           default:
-    //             lineData.emptyLocations.push(rowSequence);
-    //         } // end switch
-    //       } // lineData object populated.
-
-    //       // send data to line solver, and save data it returns to solution array
-    //       const newMarks = LineSolver(lineData);
-    //       newMarks.data.forEach((value, location) => {
-    //         if (location !== 0) {
-    //           const cell = location + 5 * row; // calculate cell index
-    //           const column = location; // calculate column index
-    //           solution.splice(cell, 1, value); // place an X or a blot
-    //           headerData.needsUpdate.splice(column, 1, true); // mark the column for an update
-    //         } // no change in this location.
-    //       }); // finished cycling through newMarks Data
-    //       if (newMarks.complete) headerData.complete.splice(row, 1, true); // mark the row complete
-    //     } // row didn't need update.
-    //   } // finished passing through rows
-    // } // pass through columns and rows again
-
-    // // capture and click grid cells
     // console.log('Start Clicking in the solution');
-    // cy.get('[class="cell"]').then(($cellGrid) => {
-    //   console.log(solution);
-    //   for (let i = 0; i < solution.length; i += 1) {
-    //     switch (solution[i]) {
-    //       case -1:
-    //         cy.get($cellGrid[i]).rightclick();
-    //         break;
-    //       case 1:
-    //         cy.get($cellGrid[i]).click();
-    //         break;
-    //       default:
-    //     }
+    // cy.get('.cell').as('cells')
+    // for (let row = 0; row < puzzle.dimension; row++) {
+    //   for (let column = 0; column < puzzle.dimension; column++){
+    //     console.log({row, column, value:solution[row][column], cellNum: row*puzzle.dimension + column})
+    //     if(solution[row][column] === 1) cy.get('@cells').eq(row*puzzle.dimension + column).click()
+
     //   }
-    // });
+    // }
   });
   // // click submit
   // cy.get('[id=btnReady]').click();
